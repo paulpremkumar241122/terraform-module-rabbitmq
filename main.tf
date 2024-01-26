e733c3650c2b40f7a8f2129c4d2369b44ea9a22f
Paul Prem Kumar <paul.prem5@gmail.com>
2024-01-26 06:15:51 +0530

    rabbaitmq module

---
+++ main.tf
@@ -0,0 +1,43 @@
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    #description      = "SSH"
+    from_port        = 5672
+    to_port          = 5672
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_subnets_cidr
+  }
+
+  ingress {
+    #description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_ssh_cidr
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.sg.id ]
+  subnet_id = var.subnet_id
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  tags = merge({ Name = "${var.component}-${var.env}" }, var.tags)
+}
+
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {}
+variable "component" {}
+variable "env" {}
+variable "sg_subnets_cidr" {}
+variable "tags" {}
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "allow_ssh_cidr" {}
